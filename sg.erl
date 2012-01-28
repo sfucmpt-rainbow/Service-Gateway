4e097fcd69f1944eee93078802f83cd0994880d7
Fred Shih <ffred@sfu.ca>
2012-01-27 17:04:38 -0800

    Added echoing code. SG will now echo strings from telnet connections.

--- sg.erl
+++ sg.erl
@@ -36,9 +36,18 @@ worker(Server, Listen) ->
 	case gen_tcp:accept(Listen) of
 		{ok, Socket} ->
 			Server ! next_worker,
-			gen_tcp:send(Socket, io_lib:format("greetings", [])),
-			gen_tcp:close(Socket);
+			echo(Socket);
 		{error, Reason} ->
 			Server ! next_worker,
 			io:format("Can't accept socket ~p~n", [Reason])
 	end.
+
+echo(Socket) ->
+	case gen_tcp:recv(Socket, 0) of
+		{ok, Packet} ->
+			gen_tcp:send(Socket, Packet),
+			gen_tcp:close(Socket);
+		{error, Reason} ->
+			io:format("Packet rejected because: ~p~n", [Reason])
+	end.
+
